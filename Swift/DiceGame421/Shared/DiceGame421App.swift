ea69bb197335f2c18dddd21ce932cde9ba36975c
nicolas <nicolas.savoini@mac.com>
2020-07-04 11:40:40 -0400

    Create Swift Project

---
+++ Swift/DiceGame421/Shared/DiceGame421App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGame421App.swift
+//  Shared
+//
+//  Created by Nicolas Savoini on 2020-07-04.
+//
+
+import SwiftUI
+
+@main
+struct DiceGame421App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift/DiceGame421/Shared/Model/Dice.swift
@@ -0,0 +1,13 @@
+//
+//  Dice.swift
+//  iOS
+//
+//  Created by Nicolas Savoini on 2020-07-04.
+//
+
+import Foundation
+
+struct Dice {
+    var face = 6
+    var value = 1
+}
